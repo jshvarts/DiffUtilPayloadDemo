5e533a1d54cb03bb89160199321afaab0d810277
James Shvarts <james.shvarts@thriveglobal.com>
2021-03-07 00:06:03 -0500

    Use payloads to prevent whole item re-rendering

--- app/src/main/java/io/valueof/diffutil/ui/main/ItemAdapter.kt
+++ app/src/main/java/io/valueof/diffutil/ui/main/ItemAdapter.kt
@@ -24,6 +24,16 @@ class ItemAdapter(
   override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
     holder.bind(getItem(position))
   }
+
+  override fun onBindViewHolder(holder: ItemViewHolder, position: Int, payloads: MutableList<Any>) {
+    if (payloads.isEmpty()) {
+      super.onBindViewHolder(holder, position, payloads)
+    } else {
+      if (payloads[0] == true) {
+        holder.bindFavoriteState(getItem(position).isFavorite)
+      }
+    }
+  }
 }
 
 class ItemViewHolder(
@@ -49,6 +59,10 @@ class ItemViewHolder(
     binding.description.text = item.description
     binding.favoriteIcon.isSelected = item.isFavorite
   }
+
+  fun bindFavoriteState(isFavorite: Boolean) {
+    binding.favoriteIcon.isSelected = isFavorite
+  }
 }
 
 class ItemDiffCallback : DiffUtil.ItemCallback<Item>() {
@@ -58,6 +72,10 @@ class ItemDiffCallback : DiffUtil.ItemCallback<Item>() {
 
   override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean {
     return oldItem == newItem
+  }
+
+  override fun getChangePayload(oldItem: Item, newItem: Item): Any? {
+    return if (oldItem.isFavorite != newItem.isFavorite) true else null
   }
 }
 
